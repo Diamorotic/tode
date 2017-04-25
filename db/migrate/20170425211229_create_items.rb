fb6f4032630fa8cc39502614628cbee8734dc5d5
Diamorotic <diamorotic@gmail.com>
2017-04-26 00:30:10 +0200

    Poprawka
    2 pliki zostały bez commita

--- db/migrate/20170425211229_create_items.rb
+++ db/migrate/20170425211229_create_items.rb
@@ -1,6 +1,7 @@
 class CreateItems < ActiveRecord::Migration[5.0]
   def change
     create_table :items do |t|
+      t.boolean :done
       t.string :name
       t.references :list, foreign_key: true
 
