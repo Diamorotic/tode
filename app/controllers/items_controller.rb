3ea01c2b8f885241cd84594b670ec26f2993c8e6
Diamorotic <diamorotic@gmail.com>
2017-04-26 14:00:52 +0200

    Checking list items
    Marking list items now works.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,19 +1,40 @@
 class ItemsController < ApplicationController
 	
+	def show
+		change_bool
+		@item.save
+		redirect_to edit_user_list_path(params[:user_id], params[:list_id])
+	end
+	
 	def new
 		@item = Item.new
 	end
 	
 	def create
-		@user = User.find_by_id(current_user[:id])
-		@list = @user.lists.find_by_id(params[:list_id])
+		fill_user_list
 		@item = @list.items.create(item_params)
 		
 		if @item.save
 			redirect_to edit_user_list_path(@user.id, @list.id)
-		else
+		else#@TODO wywalić
 			render edit_user_list_path(@user.id, @list.id)
 		end
+	end
+	
+	def update
+		change_bool
+		@item.save
+		redirect_to edit_user_list_path(@user.id, @list.id)
+	end
+	
+	def fill_user_list
+		@user = User.find_by_id(current_user[:id])
+		@list = @user.lists.find_by_id(params[:list_id])
+	end
+	
+	def change_bool
+		@item = Item.find_by_id(params[:id])
+		@item.done = !@item.done
 	end
 	
 	private
